9c525e3c3a136b2f3761e641a05b2e91950c80ba
Leon <815341194@qq.com>
2026-01-02 13:49:29 +0800

    use string to create Shaders

--- App/App.cpp
+++ App/App.cpp
@@ -2,6 +2,46 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	// Error handling
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cerr << "Failed to compile "
+            << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+            << " shader!" << std::endl;
+        std::cerr << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+	}
+	return id;
+}
+
+static unsigned int CreateProgram(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER,vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER,fragmentShader);
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -9,10 +49,6 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
-
-
-    
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -45,10 +81,30 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	// 把数据copy到GPU
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), (void*)positions, GL_STATIC_DRAW);
-	// 告诉GPU buffer数据格式
+    // 使能顶点属性
+	glEnableVertexAttribArray(0);
+    // 告诉GPU buffer数据格式
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-	// 使能顶点属性
-    glDisableVertexAttribArray(0);
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position; \n"
+        "void main() \n"
+        "{ \n"
+        "   gl_Position = position; \n"
+        "} \n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "layout(location = 0) out vec4 color; \n"
+        "void main() \n"
+        "{ \n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0); \n"
+        "} \n";
+
+	unsigned int program = CreateProgram(vertexShader, fragmentShader);
+	glUseProgram(program);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -65,6 +121,7 @@ int main(void)
         glfwPollEvents();
     }
 
+	glDeleteProgram(program);
     glfwTerminate();
     return 0;
 }
